09115ff8dd7b562588325bf873f951ff562d85d1
l.travers55@gmail.com <44858@PC004142.coll.lr.local>
2014-09-12 14:34:41 +0100

    multiplication and division of three integers

---
+++ multiplication and division.py
@@ -0,0 +1,11 @@
+#Lewis Travers
+#12/09/2014
+#Multiplying and dividing integers
+
+first_integer = int(input("Please enter your first integer: ")
+second_integer = int(input("Please enter an integer that you would like the first to be multiplied by: "))
+third_integer  = int(input("Please enter an integer that you would like the total of the previous to be divided by: "))
+
+end_total = (first_integer * second_integer) / third_integer
+print("The total is {0}").format(end_total)
+                    
