c54bef359412599590bd41881bf98c8fd27ec8be
l.travers55@gmail.com <44858@PC004142.coll.lr.local>
2014-09-12 14:13:26 +0100

    adding four integers together to a total

---
+++ adding 4 integers.py
@@ -0,0 +1,11 @@
+#Lewis Travers
+#12/09/14
+#Revision Exercises
+
+first_integer = int(input("Please enter your first integer: "))
+second_integer = int(input("Please enter your second integer: "))
+third_integer = int(input("Please enter your second integer: "))
+fourth_integer = int(input("Please enter your second integer: "))
+
+final_total = first_integer + second_integer + third_integer + fourth_integer
+print("The total is {0} ".format(final_total))
