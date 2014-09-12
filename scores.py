0d8ccc1b3dc5dd553e49b325eaa76980eca8e2bc
l.travers55@gmail.com <44858@PC004142.coll.lr.local>
2014-09-12 14:04:27 +0100

    c

---
+++ scores.py
@@ -0,0 +1,8 @@
+#Lewis Travers
+#12-09-14
+#Exercise - Example 3
+
+first_score = int(input("Please enter your first score: "))
+second_score = int(input("Please enter your first score: "))
+print("The total core is {0}+{1}".format(first_score, second_score: ")
+                                         
