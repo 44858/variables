706a5435e909a53178e201ec3c16f4b7475e85a7
l.travers55@gmail.com <44858@PC004142.coll.lr.local>
2014-09-16 12:25:01 +0100

    whole number division with the remainder

---
+++ division with remainder.py
@@ -0,0 +1,14 @@
+#Lewis Travers
+#16/09/2014
+#Division
+
+first_integer = int(input("Please enter an integer: "))
+second_integer = int(input("Please enter an integer that you would like the first to be divided by: "))
+
+total = first_integer // second_integer
+
+remainder = first_integer % second_integer
+                     
+print("The answer is {0} and the remainder is {1} ".format(total,remainder))
+                     
+                     
