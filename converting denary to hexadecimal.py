1115ebd73421d4cba6aa5e4e8c803e6356fb2d98
l.travers55@gmail.com <44858@PC004142.coll.lr.local>
2014-09-18 10:07:54 +0100

    converting a decimal number into a binary number

---
+++ converting denary to hexadecimal.py
@@ -0,0 +1,10 @@
+#Lewis Travers
+#18/09/2014
+#Converting denary to hexadecimal
+
+denary_value = int(input("Please enter a denary number: "))
+binary_string = ""
+while denary_value > 0:
+    binary_string = str(denary_value%2) + binary_string
+    denary_value = denary_value // 2
+print("The binary equivalent is {0}".format(binary_string))
