44039fcf830724d7446521648081805f627b9edf
l.travers55@gmail.com <44858@PC004142.coll.lr.local>
2014-09-19 14:29:25 +0100

    Converting a fahrenheit temperature into centigrade

---
+++ degrees F into C.py
@@ -0,0 +1,10 @@
+#Lewis Travers
+#19/09/2014
+#Degrees fahrenheit into centigrade
+
+degrees_fahren = int(input("Please enter a temperature in degrees Fahrenheit: "))
+
+degrees_cent = (degrees_fahren - 32) * (5/9)
+
+print("The temperature in centigrade is {0} ".format(degrees_cent))
+
